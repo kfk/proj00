f5043b4001b9a24c45cc25713a4398201bd39c7f
kfk <alessiocivitillo@gmail.com>
2011-11-13 16:32:31 +0100

    added project file

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject proj00 "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.2.1"]
+                           [org.clojure/clojure-contrib "1.2.0"]
+                           [sqlitejdbc "0.5.6"]
+                           [noir "1.1.0"]
+                           [incanter "1.2.3-SNAPSHOT"]]
+            :main proj00.server)
+
